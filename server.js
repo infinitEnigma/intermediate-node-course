4715fe2770342d6bf01d61038458245249aa4a48
Jovan Stojanovic <J.Stojanovic@protonmail.com>
2021-01-25 10:33:13 +0100

    Update server.js
    Add rate limiter

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const bodyParser= require('body-parser');
 const port=8000;
 const app= express();
 const User=require('./models/User');
+
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
+
 mongoose.connect('mongodb://localhost/userData')
 app.use(bodyParser.json());
 
@@ -28,6 +36,9 @@ function sendResponse(res,err,data){
     })
   }
 }
+// apply rate limiter to all requests
+app.use(limiter);
+
 app.post('/users',(req,res)=>{
   User.create(
     {...req.body.newData},
